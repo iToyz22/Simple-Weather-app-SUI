72f2650f37a0cdc3f7686e70961445808661fb71
Pogorelov Anatoliy <middle-earth2@yandex.ru>
2022-09-29 00:23:43 +0300

    Initial Commit

---
+++ iOS Weather SwiftUI App/iOS_Weather_SwiftUI_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_Weather_SwiftUI_AppApp.swift
+//  iOS Weather SwiftUI App
+//
+//  Created by Anatoliy on 29.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct iOS_Weather_SwiftUI_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
